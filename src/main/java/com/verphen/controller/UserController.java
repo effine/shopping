c71c89572b5ffbafb74c8ff342519737b52a77de
verphen <verphen@163.com>
2014-11-04 22:58:56 +0800

    perfect package

---
+++ src/main/java/com/verphen/controller/UserController.java
@@ -0,0 +1,13 @@
+
+/**
+ * @author verphen
+ * @date 2014年11月4日  下午10:55:05
+ */
+
+package com.verphen.controller;
+
+public class UserController {
+
+}
+
+
---
+++ src/main/java/com/verphen/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+
+/**
+ * @author verphen
+ * @date 2014年11月4日  下午10:56:50
+ */
+
+package com.verphen.service.impl;
+
+public class UserServiceImpl {
+
+}
+
+
