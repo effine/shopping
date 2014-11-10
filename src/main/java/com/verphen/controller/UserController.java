fccefc6c3f405bfac0fbece1404a704d70380bbf
verphen <verphen@163.com>
2014-11-10 22:36:44 +0800

    update

--- src/main/java/com/verphen/controller/UserController.java
+++ src/main/java/com/verphen/controller/UserController.java
@@ -1,4 +1,3 @@
-
 /**
  * @author verphen
  * @date 2014年11月4日  下午10:55:05
@@ -6,8 +5,14 @@
 
 package com.verphen.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class UserController {
 
+	public String login(HttpServletRequest request, HttpServletResponse response) {
+
+		return null;
+	}
+
 }
-
-
