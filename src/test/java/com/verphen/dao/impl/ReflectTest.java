08be9c6f12ab80ce6eabf991357a9c2b682d1fe5
verphen <verphen@163.com>
2014-12-29 22:49:55 +0800

    update

--- src/test/java/com/verphen/dao/impl/ReflectTest.java
+++ src/test/java/com/verphen/dao/impl/ReflectTest.java
@@ -5,26 +5,10 @@
 
 package com.verphen.dao.impl;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class ReflectTest {
 	public static void main(String[] args) {
-		 
-		List<String> list = new ArrayList<String>();
-		Class c = list.getClass();
-		Class c1 = List.class;
-		try {
-			Class c2 = Class.forName("java.util.List");
-			System.err.println(c==c1);
-			System.err.println(c2==c1);
-			System.err.println("========");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		
-		
-		
+
 	}
 }
 
