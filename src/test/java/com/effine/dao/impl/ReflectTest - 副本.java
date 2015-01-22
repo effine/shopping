cbfadf32eaec131af6081ef53bc0781c631c89c7
verphen <verphen@163.com>
2015-01-22 23:04:17 +0800

    add unrelated files

---
+++ src/test/java/com/effine/dao/impl/ReflectTest - 副本.java
@@ -0,0 +1,30 @@
+/**
+ * @author verphen
+ * @date 2014年12月9日  上午12:41:10
+ */
+
+package com.effine.dao.impl;
+
+
+public class ReflectTest {
+	public static void main(String[] args) {
+
+	}
+}
+
+class ReflectTest1 {
+
+	public static void say() {
+		//---------------------
+		//---------------------
+		//---------------------
+		//---------------------
+	}
+
+
+	public String toSay(){
+
+		System.out.println("To say !");
+	}
+
+}
