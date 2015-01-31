35702ecba5e7c329848364e06cfe9c415ca08fdf
verphen <verphen@163.com>
2015-01-31 22:59:07 +0800

    update

--- src/main/java/com/effine/redis/TestRedis.java
+++ src/main/java/com/effine/redis/TestRedis.java
@@ -1,4 +1,3 @@
-
 /**
  * @author verphen
  * @date 2015年1月31日  下午4:53:57
@@ -6,8 +5,22 @@
 
 package com.effine.redis;
 
+import redis.clients.jedis.Jedis;
+
 public class TestRedis {
+	static String constr = "192.168.168.165";
 
+	public static Jedis getRedis() {
+		Jedis jedis = new Jedis(constr);
+		return jedis;
+	}
+
+	public static void main(String[] args) {
+		Jedis j = TestRedis.getRedis();
+		String output;
+		j.set("hello", "world");
+		output = j.get("hello");
+		System.out.println(output);
+
+	}
 }
-
-
