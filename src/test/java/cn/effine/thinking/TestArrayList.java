2ff0e532574c4ee758c9b2c49eb1f9e5039fcddd
verphen <verphen@163.com>
2015-05-08 18:03:20 +0800

    add test arraylist

---
+++ src/test/java/cn/effine/thinking/TestArrayList.java
@@ -0,0 +1,34 @@
+/**
+ * @author effine
+ * @Date 2015年5月8日  下午1:47:28
+ * @email verphen#gmail.com
+ */
+
+package cn.effine.thinking;
+
+/**
+ * 简易版ArrayList
+ */
+class ArrList {
+	private Object obj;
+
+	public Object getObj() {
+		return obj;
+	}
+
+	public void add(Object obj) {
+		this.obj = obj;
+	}
+
+}
+
+public class TestArrayList {
+	public static void main(String[] args) {
+		ArrList arrList = new ArrList();
+		arrList.add(1);
+		arrList.add("1");
+
+		Integer objInt = (Integer) arrList.getObj();
+		System.out.println(objInt);
+	}
+}
