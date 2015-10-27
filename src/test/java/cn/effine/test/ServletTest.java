410a06f1cc35f7663387fd6bfecc5bcf0c58e6a2
effine-mac <verphen@163.com>
2015-10-27 22:21:35 +0800

    update

---
+++ src/test/java/cn/effine/test/ServletTest.java
@@ -0,0 +1,17 @@
+
+/**
+ * @author effine
+ * @Date 2015年10月26日  下午10:09:31
+ * @email verphen#gmail.com
+ * @site http://www.effine.cn
+ */
+
+package cn.effine.test;
+
+import javax.servlet.http.HttpServlet;
+
+public class ServletTest extends HttpServlet{
+
+}
+
+
