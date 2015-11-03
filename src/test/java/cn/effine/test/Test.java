138ba5d1175202a256ba168e294209539e60632f
effine <verphen@163.com>
2015-11-03 11:14:12 +0800

    test Thread

--- src/test/java/cn/effine/test/Test.java
+++ src/test/java/cn/effine/test/Test.java
@@ -7,11 +7,22 @@
 
 package cn.effine.test;
 
+import java.util.ArrayList;
+
 public class Test {
 
 	public static void main(String[] args) {
+		Class c1 = new ArrayList<Integer>().getClass();
+		Class c2 = new ArrayList<String>().getClass();
+		System.out.println(c1 == c2);
 
-		Container container = new Container("name", "effine");
-
+		Object obj = new Object();
+		Integer in = new Integer(1);
+		in.toString();
 	}
+}
+
+class ThreadTest extends Thread{
+	
+	
 }
