7bbe3706e44564d15030bfaf98ccdc76996904cb
effine <verphen@163.com>
2015-11-04 16:19:46 +0800

    add jdk annotation test

---
+++ src/test/java/cn/effine/test/MethodInfo.java
@@ -0,0 +1,29 @@
+/**
+ * @author effine
+ * @Date 2015年11月4日  下午3:54:23
+ * @email verphen#gmail.com
+ * @site http://www.effine.cn
+ */
+
+package cn.effine.test;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Target(ElementType.METHOD)
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MethodInfo {
+	String author() default "Pankaj";
+
+	String date();
+
+	int revision() default 1;
+
+	String comments();
+}
