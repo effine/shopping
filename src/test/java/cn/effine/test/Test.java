2f5da72b807286bad8e1fb64cb4539bcb4a0877a
effine <verphen@163.com>
2015-11-11 18:11:08 +0800

    update TODO

--- src/test/java/cn/effine/test/Test.java
+++ src/test/java/cn/effine/test/Test.java
@@ -7,22 +7,12 @@
 
 package cn.effine.test;
 
-import java.util.ArrayList;
 
 public class Test {
 
 	public static void main(String[] args) {
-		Class c1 = new ArrayList<Integer>().getClass();
-		Class c2 = new ArrayList<String>().getClass();
-		System.out.println(c1 == c2);
-
-		Object obj = new Object();
-		Integer in = new Integer(1);
-		in.toString();
+		 Integer i = 9;
+		 String str = String.valueOf(i);
+		 String s = i.toString();
 	}
-}
-
-class ThreadTest extends Thread{
-	
-	
 }
