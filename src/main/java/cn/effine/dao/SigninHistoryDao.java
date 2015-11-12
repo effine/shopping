880a85d55d948f3ad02f342bc4d6b04b75d83b5a
effine-mac <verphen@163.com>
2015-11-12 23:16:56 +0800

    add SigninHistoryDao and SigninHistoryDaoImpl

---
+++ src/main/java/cn/effine/dao/SigninHistoryDao.java
@@ -0,0 +1,15 @@
+
+/**
+ * @author effine
+ * @Date 2015年11月12日  下午11:16:01
+ * @email verphen#gmail.com
+ * @site http://www.effine.cn
+ */
+
+package cn.effine.dao;
+
+public interface SigninHistoryDao {
+
+}
+
+
---
+++ src/main/java/cn/effine/dao/impl/SigninHistoryDaoImpl.java
@@ -0,0 +1,14 @@
+/**
+ * @author effine
+ * @Date 2015年11月12日  下午11:16:19
+ * @email verphen#gmail.com
+ * @site http://www.effine.cn
+ */
+
+package cn.effine.dao.impl;
+
+import cn.effine.dao.SigninHistoryDao;
+
+public class SigninHistoryDaoImpl implements SigninHistoryDao {
+
+}
