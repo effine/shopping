21c1c12304e3358df4e81f1541559bdcb7708b1a
effine <verphen@163.com>
2015-11-13 16:48:31 +0800

    test mybatis

---
+++ src/test/java/cn/effine/test/SigninHistoryTest.java
@@ -0,0 +1,33 @@
+/**
+ * @author effine
+ * @Date 2015年11月13日  下午4:29:08
+ * @email verphen#gmail.com
+ * @site http://www.effine.cn
+ */
+
+package cn.effine.test;
+
+import org.apache.ibatis.session.SqlSession;
+
+import cn.effine.dao.AbstractDao;
+import cn.effine.model.SigninHistory;
+
+public class SigninHistoryTest {
+
+	public static void getSigninHistory(int id) {
+		SqlSession session = AbstractDao.getSqlSession();
+		SigninHistory history = session
+				.selectOne(
+						"cn.effine.ISigninHistoryOperation.selectSigninHistoryByID",
+						id);
+		session.close();
+
+		System.out.println(history);
+
+	}
+	
+	public static void main(String[] args) {
+		
+		getSigninHistory(1);
+	}
+}
