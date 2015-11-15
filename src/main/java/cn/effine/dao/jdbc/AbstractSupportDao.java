69a5d84cef1df68844e65b910493a19269287106
effine-mac <verphen@163.com>
2015-11-15 20:58:06 +0800

    add dao/jdbc

---
+++ src/main/java/cn/effine/dao/jdbc/AbstractSupportDao.java
@@ -0,0 +1,12 @@
+/**
+ * @author effine
+ * @Date 2015年11月15日  下午8:57:34
+ * @email verphen#gmail.com
+ * @site http://www.effine.cn
+ */
+
+package cn.effine.dao.jdbc;
+
+public class AbstractSupportDao {
+
+}
