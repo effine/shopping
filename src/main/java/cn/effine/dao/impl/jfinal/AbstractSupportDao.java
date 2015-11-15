11a6ed2a3a05d59009dba5d31ebddf28f2288b34
effine-mac <verphen@163.com>
2015-11-15 21:34:19 +0800

    add jfinal mudule for daoImpl

---
+++ src/main/java/cn/effine/dao/impl/jfinal/AbstractSupportDao.java
@@ -0,0 +1,15 @@
+
+/**
+ * @author effine
+ * @Date 2015年11月15日  下午9:33:51
+ * @email verphen#gmail.com
+ * @site http://www.effine.cn
+ */
+
+package cn.effine.dao.impl.jfinal;
+
+public class AbstractSupportDao {
+
+}
+
+
