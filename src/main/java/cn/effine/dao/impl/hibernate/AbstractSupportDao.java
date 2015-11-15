3cbd4a9b80696d5cd88fbb1c5a938057b67304ee
effine-mac <verphen@163.com>
2015-11-15 21:32:44 +0800

    add hibernate AbsctractSupportDao

---
+++ src/main/java/cn/effine/dao/impl/hibernate/AbstractSupportDao.java
@@ -0,0 +1,13 @@
+/**
+ * @author effine
+ * @Date 2015年11月15日  下午8:57:34
+ * @email verphen#gmail.com
+ * @site http://www.effine.cn
+ */
+
+package cn.effine.dao.impl.hibernate;
+
+
+public class AbstractSupportDao {
+
+}
