3d8f6db129cde54980a1ab499d3c77d341b9e61f
effine <verphen@163.com>
2015-12-02 18:02:09 +0800

    add controller for test

---
+++ src/test/java/cn/effine/controller/FileController.java
@@ -0,0 +1,12 @@
+/**
+ * @author effine
+ * @Date 2015年12月2日  下午5:56:45
+ * @email verphen#gmail.com
+ * @site http://www.effine.cn
+ */
+
+package cn.effine.controller;
+
+public class FileController {
+
+}
