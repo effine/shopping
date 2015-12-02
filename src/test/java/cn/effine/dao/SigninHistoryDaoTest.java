1bf31ca81829179705df88d6b796e19641a64920
effine <verphen@163.com>
2015-12-02 17:42:04 +0800

    add test interfase

---
+++ src/test/java/cn/effine/dao/SigninHistoryDaoTest.java
@@ -0,0 +1,20 @@
+/**
+ * @author effine
+ * @Date 2015年12月2日  下午5:33:32
+ * @email verphen#gmail.com
+ * @site http://www.effine.cn
+ */
+
+package cn.effine.dao;
+
+public interface SigninHistoryDaoTest {
+
+	/**
+	 * 获取用户登录历史
+	 *
+	 * @param id
+	 *            登录历史ID
+	 * @return 历史登录信息
+	 */
+	void getSigninHistory();
+}
