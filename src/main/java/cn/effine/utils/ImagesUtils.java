64b1ae3d513f9632af008c93190699928ce255af
effine-mac <verphen@163.com>
2015-12-22 00:17:27 +0800

    add images dule

---
+++ src/main/java/cn/effine/utils/ImagesUtils.java
@@ -0,0 +1,56 @@
+
+/**
+ * @author effine
+ * @Date 2015年12月21日  下午11:43:57
+ * @email verphen#gmail.com
+ * @site http://www.effine.cn
+ */
+
+package cn.effine.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import org.apache.commons.io.IOUtils;
+
+import com.alibaba.simpleimage.ImageRender;
+import com.alibaba.simpleimage.SimpleImageException;
+import com.alibaba.simpleimage.render.ReadRender;
+import com.alibaba.simpleimage.render.ScaleParameter;
+import com.alibaba.simpleimage.render.ScaleRender;
+import com.alibaba.simpleimage.render.WriteRender;
+
+public class ImagesUtils {
+
+    public static void main(String[] args) { 
+        File in = new File("/source/test/src.JPG");      //原图片
+        File out = new File("/source/test/dist.jpg");       //目的图片
+        ScaleParameter scaleParam = new ScaleParameter(211, 400);  //将图像缩略到1024x1024以内，不足1024x1024则不做任何处理
+        
+        FileInputStream inStream = null;
+        FileOutputStream outStream = null;
+        WriteRender wr = null;
+        try {
+            inStream = new FileInputStream(in);
+            outStream = new FileOutputStream(out);
+            ImageRender rr = new ReadRender(inStream);
+            ImageRender sr = new ScaleRender(rr, scaleParam);
+            wr = new WriteRender(sr, outStream);
+        
+            wr.render();                            //触发图像处理
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            IOUtils.closeQuietly(inStream);         //图片文件输入输出流必须记得关闭
+            IOUtils.closeQuietly(outStream);
+            if (wr != null) {
+                try {
+                    wr.dispose();                   //释放simpleImage的内部资源
+                } catch (SimpleImageException ignore) {
+                    // skip ... 
+                }
+            }
+        }
+    }
+}
