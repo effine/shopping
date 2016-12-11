65450fdfa595ee332be30be0e05c19488c2a07ee
effine <iballader@gmail.com>
2016-12-12 00:16:26 +0800

    update

--- src/main/java/cn/effine/lab/DelEmptyFolder.java
+++ src/main/java/cn/effine/lab/DelEmptyFolder.java
@@ -12,38 +12,34 @@ import java.io.File;
  * 
  */
 public class DelEmptyFolder {
-	public static void main(String[] args) {
-		String path = "D:\\effine";
-		File file = new File(path);
-		if (file.exists()) {
-			if (del(file))
-				System.out.println("删除成功！");
-			else
-				System.out.println("路径：" + path + " 及其子路径没有空文件夹！");
-		} else {
-			System.out.println("文件: " + path + " 不存在!");
-		}
-	}
+
+	private DelEmptyFolder(){}
 
 	/**
 	 * 采用递归思想，级联删除空文件夹
 	 * 
-	 * @param file
+	 * @param srcFile 待操作的源文件夹
 	 * @return Boolean
 	 */
-	public static boolean del(File file) {
-		// TODO 删除子空目录，此时父空目录无法删除
-		if (file.isDirectory()) {
-			File[] files = file.listFiles();
+	public static void deleteEmptyForld(String dir) {
+		File srcFile = new File(dir);
+		if (srcFile.isDirectory()) {
+			File[] files = srcFile.listFiles();
 			if (0 != files.length) {
-				for (File f : files) {
-					del(f);
+				for (File file : files) {
+					deleteEmptyForld(file.getAbsolutePath());
 				}
 			} else {
-				file.delete();
-				return true;
+				srcFile.delete();
+				if(dir.equals(srcFile.getParent())){
+					return;
+				}
+				deleteEmptyForld(srcFile.getPath());
 			}
 		}
-		return false;
+	}
+
+	public static void main(String[] args) {
+		DelEmptyFolder.deleteEmptyForld("/sources/test/testdelete");
 	}
 }
