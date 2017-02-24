9c3cad595f0903d681e0aa78519b82b5e261b3b1
effine <effine@effine.local>
2017-02-24 18:09:23 +0800

    使用Junit及Mock调试成功

--- src/test/java/cn/effine/controller/UserControllerTest.java
+++ src/test/java/cn/effine/controller/UserControllerTest.java
@@ -1,14 +1,36 @@
 package cn.effine.controller;
 
-import org.springframework.test.web.servlet.MockMvc;
+import cn.effine.model.User;
+import cn.effine.service.UserService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by effine on 2/15/17.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class UserControllerTest {
 
-    private MockMvc mockMvc;
+    @InjectMocks
+    private UserController userController;
 
+    @Mock
+    private UserService userService;
 
+    @Test
+    public void testSignup() {
+        // TODO 下面的 mock 与 @InjectMocks 声明的区别？
+        //UserController controller = mock(UserController.class);
+//        UserService userService  = mock(UserService.class);
+        when(userService.signup(any(User.class))).thenReturn(true);
+
+        Assert.assertEquals("true", userController.signup(new User()));
     }
 }
