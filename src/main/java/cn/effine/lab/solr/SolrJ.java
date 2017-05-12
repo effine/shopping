923289335df3fbdce776e6b6c0b9cbe7ef89b9f1
zhangyafei <zhangyafei@co-mall.com>
2017-05-12 16:20:14 +0800

    更新

--- src/main/java/cn/effine/lab/solr/SolrJ.java
+++ src/main/java/cn/effine/lab/solr/SolrJ.java
@@ -1,5 +1,13 @@
 package cn.effine.lab.solr;
 
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.common.params.SolrParams;
+
+import java.io.IOException;
+import java.util.Iterator;
+
 /**
  * @author effine  Email: zhangyafei#co-mall.com
  * @version 3.2.1
@@ -7,4 +15,35 @@ package cn.effine.lab.solr;
  * @since 2017-05-10 14:51
  */
 public class SolrJ {
+
+
+    private static String url = "http://localhost:8010/solr";
+
+    public static void main(String[] args) throws IOException, SolrServerException {
+
+        SolrServer solrServer = new HttpSolrServer(url);
+
+
+        SolrParams solrParams = new SolrParams() {
+            @Override
+            public String get(String param) {
+                return null;
+            }
+
+            @Override
+            public String[] getParams(String param) {
+                return new String[0];
+            }
+
+            @Override
+            public Iterator<String> getParameterNamesIterator() {
+                return null;
+            }
+        }
+
+        // 清空索引
+        //solrServer.deleteByQuery("*:*");
+
+
+    }
 }
