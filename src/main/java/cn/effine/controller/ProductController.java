a6eaba6e82b3c7f39f286b98fd1d7e57693b9b09
effine <iballader@gmail.com>
2017-07-12 00:09:21 +0800

    添加spring-boot

---
+++ src/main/java/cn/effine/controller/ProductController.java
@@ -0,0 +1,22 @@
+package cn.effine.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author effine
+ * @Date 2017-07-09 21:21
+ * @email iballader#gmail.com
+ * @site http://www.effine.cn
+ * @sine 0.1
+ */
+
+@RestController
+@RequestMapping("product")
+public class ProductController {
+
+    @RequestMapping("say")
+    public String sayHello() {
+        return "hi, product";
+    }
+}
