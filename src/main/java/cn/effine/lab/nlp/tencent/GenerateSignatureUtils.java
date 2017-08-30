9429d463726d73b4ab210a4e992bbb1965fed106
effine <iballader@gmail.com>
2017-08-31 02:23:56 +0800

    调试腾讯云 自然语言处理接口

--- src/main/java/cn/effine/lab/nlp/tencent/GenerateSignatureUtils.java
+++ src/main/java/cn/effine/lab/nlp/tencent/GenerateSignatureUtils.java
@@ -1,10 +1,22 @@
 package cn.effine.lab.nlp.tencent;
 
+import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import sun.misc.BASE64Encoder;
 
+import java.net.URLDecoder;
 import java.net.URLEncoder;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 生产签名串工具类
@@ -70,24 +82,20 @@ public class GenerateSignatureUtils {
 
         // 字典序列排序
         Map<String, Object> paramsMap = new HashMap<>();
-        paramsMap.put("Action", "LexicalAnalysis");
+        paramsMap.put("Action", "TextSentiment");
         paramsMap.put("SecretId", SecretId);
-        long currentTime = System.currentTimeMillis();
-        paramsMap.put("Timestamp", "1504104808809");
-        System.out.println("> 当前时间毫秒数：" + currentTime);
-
-        paramsMap.put("Nonce", 345122);
+        paramsMap.put("Timestamp", System.currentTimeMillis());
+        paramsMap.put("Nonce", new Random().nextInt(10000));
         paramsMap.put("Region", "gz");
 
-        paramsMap.put("text", "我爱洗澡");
-        paramsMap.put("code", 2097152);
+        paramsMap.put("content", "双万兆服务器就是好，只是内存小点");
 
         String param = sortParam(paramsMap);
         System.out.println("> 请求字符串：" + param);
 
         // 2.3. 拼接签名原文字符串 (请求方法 + 请求主机 +请求路径 + ? + 请求字符串)
         String method = "GET";
-        String host = "cvm.api.qcloud.com";
+        String host = "https://wenzhi.api.qcloud.com";
         String path = "/v2/index.php";
 
         StringBuilder sourceStr = new StringBuilder();
@@ -99,7 +107,7 @@ public class GenerateSignatureUtils {
 
 
         // 2.4. 生成签名串
-        String signStr = HmacSHA1Encryption.HmacSHA1Encrypt(URLEncoder.encode(sourceStr.toString(), "utf-8"), SecretKey);
+        String signStr = HmacSHA1Encryption.HmacSHA1Encrypt(sourceStr.toString(), SecretKey);
         System.out.println("> 生产的签名串：" + signStr);
 
         // 使用 Base64 进行编码
@@ -111,8 +119,61 @@ public class GenerateSignatureUtils {
         signStr = URLEncoder.encode(signStr, "utf-8");
         System.out.println("> URL 编码：" + signStr);
 
+        sourceStr.append("&Signature=");
+        sourceStr.append(signStr);
+
+        String urlAndParam = sourceStr.toString().replace(method, "");
+        System.out.println("最终请求方法URL: " + urlAndParam);
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(urlAndParam);
+        CloseableHttpResponse response = httpClient.execute(httpGet);
+        HttpEntity entity = response.getEntity();
+
+
+        System.out.println("\n\n\n响应内容：" + unicodeToChina(EntityUtils.toString(entity)));
+        System.out.println("\n\n\n");
+
+        httpClient.close();
+        response.close();
+
 
     }
 
 
+    /**
+     * Unicode 转为汉字
+     *
+     * @param str
+     * @return
+     */
+    public static String unicodeToChina(String str) {
+        Charset set = Charset.forName("UTF-16");
+        Pattern p = Pattern.compile("\\\\u([0-9a-fA-F]{4})");
+        Matcher m = p.matcher(str);
+        int start = 0;
+        int start2 = 0;
+        StringBuffer sb = new StringBuffer();
+        while (m.find(start)) {
+            start2 = m.start();
+            if (start2 > start) {
+                String seg = str.substring(start, start2);
+                sb.append(seg);
+            }
+            String code = m.group(1);
+            int i = Integer.valueOf(code, 16);
+            byte[] bb = new byte[4];
+            bb[0] = (byte) ((i >> 8) & 0xFF);
+            bb[1] = (byte) (i & 0xFF);
+            ByteBuffer b = ByteBuffer.wrap(bb);
+            sb.append(String.valueOf(set.decode(b)).trim());
+            start = m.end();
+        }
+        start2 = str.length();
+        if (start2 > start) {
+            String seg = str.substring(start, start2);
+            sb.append(seg);
+        }
+        return sb.toString();
+    }
 }
